3692218e56af1af7019c7978884b873ebc3898b8
Yogii faujdar <mehar@Yogiis-MacBook-Pro.local>
2026-02-20 12:48:09 +0530

    Add icon files for PWA

---
+++ app/src/main/java/com/meharfinance/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.meharfinance.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
